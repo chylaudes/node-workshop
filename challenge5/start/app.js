5710cef17021ac9175a9f5d45d0c7511ffd3d52e
Christine Lau <chylau.design@gmail.com>
2016-09-03 23:12:03 -0700

    finished with challenge 5 made a chat app

--- challenge5/start/app.js
+++ challenge5/start/app.js
@@ -1,7 +1,6 @@
-var express = require('express'), 
-    http = require('http'), 
-    path = require('path')
-    io = require('socket.io');
+var express = require('express'),
+    http = require('http'),
+    path = require('path');
 
 var app = express();
 
@@ -25,8 +24,28 @@ var io = require('socket.io').listen(server);
 // Handle socket traffic
 io.sockets.on('connection', function (socket) {
     // Relay chat data to all clients
+    socket.on('username', function(username){ //listening for the "username" connection
+    console.log("firstlevel",username);
+      socket.set('username', username); // persistent cookie, like storing a value throughout
+    });
     socket.on('chat', function(data) {
-        socket.emit('chat',data);
-        socket.broadcast.emit('chat', data);
+      socket.get('username', function(err, username) {
+        console.log("2ndlevel",username);
+
+        var user = err ? "Anon" : username;
+        console.log("user", user);
+        var payload = {
+          message: data.message,
+          username: user
+        };
+
+        // io.sockets.emit('chat', payload); //condenses the last two lines of code
+
+
+        socket.emit('chat', payload); // this will write back
+        socket.broadcast.emit('chat', payload); // this will broadcast all connected clients
+
+      });
+
     });
 });
